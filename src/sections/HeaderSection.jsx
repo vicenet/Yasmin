7e02d8ee84a32a485a9944ed1869e6f1709ca017
vicenet <gitaubrian425@gmail.com>
2025-05-17 05:00:20 +0300

    build

--- src/sections/HeaderSection.jsx
+++ src/sections/HeaderSection.jsx
@@ -8,8 +8,8 @@ const HeaderSection = () => {
         <nav className="main-nav">
           <ul className="nav-left">
             <li><a href="/" className="nav-link">Home</a></li>
-            <li><a href="/about" className="nav-link">About</a></li>
-            <li><a href="/services" className="nav-link">Services</a></li>
+            <li><a href="/" className="nav-link">About</a></li>
+            <li><a href="/" className="nav-link">Services</a></li>
           </ul>
           
           <div className="logo">
@@ -19,9 +19,9 @@ const HeaderSection = () => {
           </div>
           
           <ul className="nav-right">
-            <li><a href="/resources" className="nav-link">Resources</a></li>
-            <li><a href="/faq" className="nav-link">FAQ</a></li>
-            <li><a href="/contact" className="nav-link">Contact</a></li>
+            <li><a href="/" className="nav-link">Resources</a></li>
+            <li><a href="/" className="nav-link">FAQ</a></li>
+            <li><a href="/" className="nav-link">Contact</a></li>
           </ul>
         </nav>
       </div>
