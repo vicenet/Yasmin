41d32784fb218b5ae5d02de2ca72b93740d9874c
vicenet <gitaubrian425@gmail.com>
2025-05-25 04:49:12 +0300

    css fixes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '192.168.101.101',
+    port: 3000
+  },
 })
